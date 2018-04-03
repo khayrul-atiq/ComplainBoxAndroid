a0c789973d35d7b17d42e39d9c847a883815e2f9
khayrulislam <bsse0822@iit.du.ac.bd>
2018-04-04 00:09:22 +0600

    home flip and button ADD

--- ComplainBox/app/src/main/java/com/example/olife/complainbox/MainActivity.java
+++ ComplainBox/app/src/main/java/com/example/olife/complainbox/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.olife.complainbox;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,13 +15,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private HashMap < Integer, Fragment > fragmentMap = new HashMap< Integer, Fragment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeFragmentMap();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +47,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        displaySelectedFragment(R.id.nav_home);
     }
 
     @Override
@@ -77,31 +86,34 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_problem_submission) {
+        displaySelectedFragment(item.getItemId());
 
-        } else if (id == R.id.nav_emergency_support) {
+        return true;
+    }
+    private void displaySelectedFragment(int item){
 
-        } else if (id == R.id.nav_event) {
+        Fragment fragment = fragmentMap.get(item);
 
-        } else if (id == R.id.nav_notice) {
-
-        } else if (id == R.id.nav_application_form) {
-
-        }
-        else if(id==R.id.nav_my_profile){
-
-        }
-        else if(id==R.id.nav_sign_out){
-
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+    }
+
+
+    private void initializeFragmentMap(){
+        fragmentMap.put(R.id.nav_home, new Home());
+        fragmentMap.put(R.id.nav_application_form,new ApplicationForm());
+        fragmentMap.put(R.id.nav_emergency_support, new EmergencySupport());
+        fragmentMap.put(R.id.nav_event,new Event());
+        fragmentMap.put(R.id.nav_my_profile, new MyProfile());
+        fragmentMap.put(R.id.nav_notice, new Notice());
+        fragmentMap.put(R.id.nav_problem_submission, new ProblemSubmission());
+
     }
 }
